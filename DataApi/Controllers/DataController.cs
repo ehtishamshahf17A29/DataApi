8f9695404a15d51a22cdd60fbde7f47e8ca640c7
ehtishamf17A29 <bscsf17a29@gmail.com>
2020-10-19 03:12:07 +0500

    Add project files.

---
+++ DataApi/Controllers/DataController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DataApi.Models;
+
+namespace DataApi.Controllers
+{
+    public class DataController : Controller
+    {
+        private readonly DataContext _context;
+
+        public DataController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Data
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TodoItems.ToListAsync());
+        }
+
+        // GET: Data/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var data = await _context.TodoItems
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+
+            return View(data);
+        }
+
+        // GET: Data/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Data/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,IsComplete")] Data data)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(data);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(data);
+        }
+
+        // GET: Data/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var data = await _context.TodoItems.FindAsync(id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return View(data);
+        }
+
+        // POST: Data/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Name,IsComplete")] Data data)
+        {
+            if (id != data.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(data);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DataExists(data.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(data);
+        }
+
+        // GET: Data/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var data = await _context.TodoItems
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+
+            return View(data);
+        }
+
+        // POST: Data/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var data = await _context.TodoItems.FindAsync(id);
+            _context.TodoItems.Remove(data);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DataExists(long id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DataApi/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataApi.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Data> TodoItems { get; set; }
+    }
+}
